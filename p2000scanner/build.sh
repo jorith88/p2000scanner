53ed9bd047e69afd3eba05a8e6c9c63c99b9be15
Jorith van den Heuvel <jorith@gmail.com>
2017-01-11 22:13:24 +0000

    add rtlfm-to-scanner to build.sh script and add start script rtl-scanner.sh

--- p2000scanner/build.sh
+++ p2000scanner/build.sh
@@ -4,4 +4,7 @@ echo "Building scanner";
 gcc -std=c99 -Wall -lm -o scanner scanner.c -ljson -lzmq -pthread -I/usr/include/json/
 gcc -std=c99 -Wall -lm -o scanner_stdin scanner_stdin.c -ljson -lzmq -pthread -I/usr/include/json/
 
+echo "Building rtlfm-to-scanner"
+gcc -std=c99 -Wall -lm -o rtlfm-to-scanner rtlfm-to-scanner.c
+
 exit 0;
---
+++ p2000scanner/rtl-scanner.sh
@@ -0,0 +1,1 @@
+rtl_fm -M fm -f 169.650M -E dc -s 24000 -g 10 | ./rtlfm-to-scanner | ./scanner_stdin
